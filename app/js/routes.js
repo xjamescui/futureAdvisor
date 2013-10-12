c0f5618305c84e3ea6aea22550e947a7c4312208
James Cui <james.q.cui@gmail.com>
2013-10-12 17:23:00 -0400

    routing cleanup

--- app/js/routes.js
+++ app/js/routes.js
@@ -1,6 +1,11 @@
-window.app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/main.html', 
-  	controller: 'mainController'});
-  // $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+window.app.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.when('/', {
+			templateUrl: 'partials/main.html',
+			controller: 'mainController'
+		});
+		$routeProvider.otherwise({
+			redirectTo: '/'
+		});
+	}
+]);
